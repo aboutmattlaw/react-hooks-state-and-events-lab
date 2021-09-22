9eb44d874891725b1dcac0cd196956b302705452
Matt <aboutmattlaw@gmail.com>
2021-09-22 09:23:59 -0400

    commit tree 9c5b16c13cc1b3197c71755a2af1316a0d90100e

--- src/components/ShoppingList.js
+++ src/components/ShoppingList.js
@@ -1,11 +1,24 @@
-import React from "react";
+import React, {useState} from "react";
 import Item from "./Item";
 
 function ShoppingList({ items }) {
+
+  const [selectedCategory, setSelectedCategory] = useState("All");
+
+  function handledCategoryChange(e){
+    setSelectedCategory(even.target.value)
+  }
+
+  const itemstoDisplay = items.filter((item) => {
+    if (selectedCategory === "All") return true;
+
+    return item.category === selectedCategory;
+  });
+
   return (
     <div className="ShoppingList">
       <div className="Filter">
-        <select name="filter">
+        <select name="filter" onChange={handledCategoryChange}>
           <option value="All">Filter by category</option>
           <option value="Produce">Produce</option>
           <option value="Dairy">Dairy</option>
